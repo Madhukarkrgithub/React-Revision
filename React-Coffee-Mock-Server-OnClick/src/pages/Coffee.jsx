5ee300fe0db87d3ef71f8cde560c02172c63792c
Madhukar kumar <madhukarkumar2468@gmail.com>
2024-03-01 07:41:11 +0530

    code

---
+++ React-Coffee-Mock-Server-OnClick/src/pages/Coffee.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Coffee = () => {
+  return (
+    <div>
+      {/* Create Button `Get Coffee` here */}
+
+      <div className="coffee_container">
+        {/* Populate coffee data inside CoffeeCard.jsx */}
+      </div>
+    </div>
+  );
+};
+
+export default Coffee;
